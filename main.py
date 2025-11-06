4dab8cbc4c8f39c350780e4ecbf1f4caf6f6d732
Matheus Ramos <matheussoaresramos@gmail.com>
2025-11-06 14:03:19 -0300

    Exercicio 3 finalizado!

--- main.py
+++ main.py
@@ -21,3 +21,11 @@ def filtrar_valores(lista_valores: List[float], limite: float) -> list:
 lista : List[float] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 limite = 6
 print(filtrar_valores(lista, limite))
+
+# EX 3
+
+def unicos(lista_valores: List[int]) -> int:
+    return len(set(lista_valores))
+
+lista : List[float] = [1, 2, 3, 4, 5, 5, 7, 8, 9, 10]
+print(unicos(lista))
