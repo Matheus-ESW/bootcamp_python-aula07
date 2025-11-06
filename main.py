1e85b50329945581a34e2f74f8f3b79912500553
Matheus Ramos <matheussoaresramos@gmail.com>
2025-11-06 13:56:23 -0300

    Exercicio 2 finalizado!

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ def media_lista(lista_valores: List[float]) -> float:
 lista : List[float] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(media_lista(lista))
 
+# EX 2
+
+def filtrar_valores(lista_valores: List[float], limite: float) -> list:
+    valores_filtrados = []
+    for valores in lista_valores:
+        if valores > limite:
+            valores_filtrados.append(valores)
+    
+    return valores_filtrados
+
+lista : List[float] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+limite = 6
+print(filtrar_valores(lista, limite))
